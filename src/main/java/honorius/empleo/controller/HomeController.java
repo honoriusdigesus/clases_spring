ff379a844229b531ceff7ab1ee95b986a52d7c14
Honorius di Gesus <honoriusdigesus@gmail.com>
2023-11-13 21:51:58 -0500

    Trabajando con listas en Thymeleaf

--- src/main/java/honorius/empleo/controller/HomeController.java
+++ src/main/java/honorius/empleo/controller/HomeController.java
@@ -1,22 +1,34 @@
 package honorius.empleo.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String mostrarHome(Model model) {
-        model.addAttribute("mensaje", "Hello Word - Spring Boot");
-        model.addAttribute("fecha",new Date());
-        String nombre = "Honorius di Gesus";
-        Integer edad = 24;
-        model.addAttribute("nombre", nombre);
-        model.addAttribute("edad", edad);
+//    @GetMapping("/")
+//    public String mostrarHome(Model model) {
+//        model.addAttribute("mensaje", "Hello Word - Spring Boot");
+//        model.addAttribute("fecha",new Date());
+//        String nombre = "Honorius di Gesus";
+//        Integer edad = 24;
+//        model.addAttribute("nombre", nombre);
+//        model.addAttribute("edad", edad);
+//        return "home";
+//    }
 
-        return "home";
+    @GetMapping("/listado")
+    public String mostrarListado(Model model) {
+        List<String> lista = new LinkedList<String>();
+        lista.add("Ingeniero electrónico");
+        lista.add("Diseñador gráfico");
+        lista.add("Contador público");
+        model.addAttribute("listaEmpleos", lista);
+        return "listado";
     }
 }

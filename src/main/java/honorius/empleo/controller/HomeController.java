427e05bc134a76e100304226684345acb79d3d25
Honorius di Gesus <honoriusdigesus@gmail.com>
2023-11-13 22:09:56 -0500

    Trabajando con objetos, para el ejemplo tenemos uno tipo Vacante

--- src/main/java/honorius/empleo/controller/HomeController.java
+++ src/main/java/honorius/empleo/controller/HomeController.java
@@ -1,5 +1,6 @@
 package honorius.empleo.controller;
 
+import honorius.empleo.model.Vacante;
 import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,16 +12,16 @@ import java.util.List;
 
 @Controller
 public class HomeController {
-//    @GetMapping("/")
-//    public String mostrarHome(Model model) {
-//        model.addAttribute("mensaje", "Hello Word - Spring Boot");
-//        model.addAttribute("fecha",new Date());
-//        String nombre = "Honorius di Gesus";
-//        Integer edad = 24;
-//        model.addAttribute("nombre", nombre);
-//        model.addAttribute("edad", edad);
-//        return "home";
-//    }
+    @GetMapping("/")
+    public String mostrarHome(Model model) {
+        model.addAttribute("mensaje", "Hello Word - Spring Boot");
+        model.addAttribute("fecha", new Date());
+        String nombre = "Honorius di Gesus";
+        Integer edad = 24;
+        model.addAttribute("nombre", nombre);
+        model.addAttribute("edad", edad);
+        return "home";
+    }
 
     @GetMapping("/listado")
     public String mostrarListado(Model model) {
@@ -30,5 +31,16 @@ public class HomeController {
         lista.add("Contador público");
         model.addAttribute("listaEmpleos", lista);
         return "listado";
+    }
+
+    @GetMapping("/vacantes")
+    public String mostrarDetalle(Model model) {
+        Vacante vacante = new Vacante();
+        vacante.setNombre("Ingeniero de datos");
+        vacante.setDescripcion("Se requiere ingeniero para analizar los datos de la empresa");
+        vacante.setFecha(new Date());
+        vacante.setSalario(150000.0);
+        model.addAttribute("vacante", vacante);
+        return "vacantes";
     }
 }

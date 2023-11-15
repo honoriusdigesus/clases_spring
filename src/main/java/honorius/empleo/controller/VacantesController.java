e6d05bd88e3c603d3ea6d6f43e14a8dfc1b6a1c1
Honorius di Gesus <honoriusdigesus@gmail.com>
2023-11-14 21:12:44 -0500

    Add botón eliminar y simulamos una eliminación

--- src/main/java/honorius/empleo/controller/VacantesController.java
+++ src/main/java/honorius/empleo/controller/VacantesController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/vacantes")
@@ -16,5 +17,12 @@ public class VacantesController {
 
         //Buscar los detalles de la vacante en la base de datos
         return "vacantes/detalles";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") int idVacante, Model model) {
+        System.out.println("Vacantes eliminada: " + idVacante);
+        model.addAttribute("idVacante", idVacante);
+        return "mensaje";
     }
 }
